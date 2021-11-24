68955c2cf29b61289c9c17ff839b13fa679d8786
Mert Altuntaş <mertfozzy@gmail.com>
2021-11-24 12:19:58 +0300

    update

--- imdb.py
+++ imdb.py
@@ -14,3 +14,12 @@ from keras.preprocessing.sequence import pad_sequences # cümle sayılarını fi
 from keras.models import Sequential # model yaratıp layerları dizi haline getireceğiz
 from keras.layers.embeddings import Embedding # yoğunluk vektörlerine dönüşüm
 from keras.layers import SimpleRNN, Dense, Activation # RNN, sınıflandırma ve sigmoid fonksiyonu
+
+(X_train, Y_train), (X_test, Y_test) = imdb.load_data(path = "imdb.npz",
+                                                       num_words = None,
+                                                       skip_top = 0,
+                                                       maxlen = None,
+                                                       seed = 113,
+                                                       start_char = 1,
+                                                       oov_char = 2,
+                                                       index_from = 3)

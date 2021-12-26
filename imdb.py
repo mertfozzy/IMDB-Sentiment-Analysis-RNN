ceec291d07a6a621946308555ab5ca1b9fc8817e
Mert Altuntaş <mertfozzy@gmail.com>
2021-12-27 01:38:15 +0300

    eda

--- imdb.py
+++ imdb.py
@@ -28,15 +28,17 @@ from keras.layers import SimpleRNN, Dense, Activation # RNN, sınıflandırma ve
 print("Type : ", type(X_train))
 print("Type : ", type(Y_train))
 
-print("X train shape: ", X_train.shape)
+#içeride ne kadar veri var? (25K)
+print("X train shape: ", X_train.shape) 
 print("Y train shape: ", Y_train.shape)
 
 
-# Exploratory Data Science = EDA
+"""=====================Exploratory Data Science = EDA======================"""
 
 #unique metodu ile Y_train ve Y_test içerisinde neler var bakıyoruz :
 print("Y train values : ", np.unique(Y_train))
 print("Y test values : ", np.unique(Y_test))
+
 
 # burada unique 0 negatif, 1 pozitif görüşler. eşit çıkıyorsa dengeli bir veri seti.
 unique, counts = np.unique(Y_train, return_counts = True)
@@ -44,6 +46,7 @@ print("Y train distribution: ", dict(zip(unique, counts)))
 
 unique, counts = np.unique(Y_test, return_counts = True)
 print("Y test distribution: ", dict(zip(unique, counts)))
+
 
 # bulduğumuz değerleri tabloya çeviriyoruz :
 plt.figure()
@@ -58,19 +61,20 @@ plt.xlabel("Classes")
 plt.ylabel("Frequency")
 plt.title("Y Test")
 
+
 # X_train içinde neler var? :
-a = X_train[0] #sıfırıncı indexi değere koyduk, içerisinde integer değerler var.
-print(a)
-print("Size of X_train[0]: ", len(a)) #toplamda kaç integer var?
+insideX = X_train[0] #sıfırıncı indexi değere koyduk, içerisinde integer değerler var.
+print(insideX)
+print("Size of X_train[0]: ", len(insideX)) #toplamda kaç integer var?
 
 
 #X_train ve X_test içerisinde dolaş, uzunlukları listeye koy. Bu bize tüm datasetteki yorumların kelime sayılarını gösterecek.
 review_len_train = []
 review_len_test = []
 
-for i, ii in zip(X_train, X_test):
-    review_len_train.append(len(i)) #X_train
-    review_len_test.append(len(ii)) #X_test
+for rev1, rev2 in zip(X_train, X_test):
+    review_len_train.append(len(rev1)) #X_train
+    review_len_test.append(len(rev2)) #X_test
     
     
 # Kelime sayılarının dağılımına bakalım, histogram çiziyoruz :
@@ -80,3 +84,32 @@ sns.distplot(review_len_test, hist_kws = {"alpha" : 0.3})
 print("Train mean : ", np.mean(review_len_train)) #histogramın orta noktası
 print("Train median : ", np.median(review_len_train)) #histogramın medyanı
 print("Train mode: ", stats.mode(review_len_train)) #histogramın en tepe noktası
+
+
+# hangi kelimeden kaç tane var : (burada kelimeler sayılara denk geliyor)
+word_index = imdb.get_word_index()
+print(type(word_index))
+print(len(word_index))
+
+
+#verilen sayının hangi kelimeye denk geldiğini bulalım : 
+for keys, values in word_index.items(): 
+    if values == 1 : #integer burada veriliyor
+        print(keys)
+
+
+#yorumu metine dönüştürme (list-comparasion method)
+# metod yazdım bu sayede yorumu parse edip olumlu olumsuz olarak da ayırt ediyoruz.
+def whatItSay(index): #default olarak 24 verdim
+   reverse_index = dict([(value,key) for (key, value) in word_index.items()])
+   decode_review = " ".join([reverse_index.get(i - 3, "!") for i in X_train[index]]) #decoding yapıyoruz
+   print(decode_review)
+   print(Y_train[index])
+   return decode_review
+
+decoded_review = whatItSay(6) # yorum indexi parantezden veriliyor
+
+"""================================End of EDA================================"""
+
+
+
